c52bb11f0bb1e900dc7462f7a2113313cf56d43e
Celes-BTGL <yurika-lover@hotmail.com>
2024-07-12 23:32:25 -0400

    commit index first

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    let credits = document.querySelector('.credits');
+    let duration = 3.5 * 60 * 1000; // 4.5 minutos en milisegundos
+    const backgroundMusic = document.getElementById('backgroundMusic');
+    const container = document.getElementById('space');
+    const iframe = document.getElementById('iframeIntroActive');
+
+    window.addEventListener('message', (event) => {
+        if (event.data == 'activeLoadFrame') {
+            container.classList.remove("hidden-page");
+            container.classList.add("show-page");
+            iframe.classList.add("hidden-page");
+            setTimeout(() => {
+                credits.style.transition = `transform ${duration}ms linear`;
+                credits.style.transform = 'translateY(-100%)';
+                backgroundMusic.play();
+
+                setTimeout(() => {
+                    credits.style.display = 'none';
+                }, duration);
+
+            }, 500);
+
+            // const reproPlay = document.getElementById("reproPlay");
+            // reproPlay.classList.remove("play")
+            // reproPlay.classList.add("pause")
+        }
+    })
+};
